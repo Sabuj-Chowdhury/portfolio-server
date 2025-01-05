8ed08bbb5d7ae75a9518fa05ff01ad6782d4c2c2
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-01-05 10:57:22 +0530

    server for my protfolio

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i53p4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = client.db("portfolioDB");
+    const ProjectCollections = db.collection("projects");
+
+    // post
+    app.post("/add", async (req, res) => {
+      const data = req.body;
+      const result = await ProjectCollections.insertOne(data);
+      res.send(result);
+    });
+
+    //get
+    app.get("/projects", async (req, res) => {
+      const result = await ProjectCollections.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await ProjectCollections.findOne(query);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("portfolio server!!!!!!!!");
+});
+app.listen(port, () => {
+  console.log(`Server running at : ${port}`);
+});
